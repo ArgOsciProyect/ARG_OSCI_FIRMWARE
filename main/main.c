226a0563060f3f0e475bd2ab4c4b9f90de0beae3
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2024-11-21 12:06:07 -0300

    No mas BLE

--- main/main.c
+++ main/main.c
@@ -1,129 +1,207 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_event.h"
-#include "nvs_flash.h"
-#include "esp_log.h"
-#include "esp_nimble_hci.h"
-#include "nimble/nimble_port.h"
-#include "nimble/nimble_port_freertos.h"
-#include "host/ble_hs.h"
-#include "services/gap/ble_svc_gap.h"
-#include "services/gatt/ble_svc_gatt.h"
-#include "sdkconfig.h"
+#include <string.h>
+#include <stdlib.h>
+#include <esp_wifi.h>
+#include <esp_event.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
+#include <lwip/sockets.h>
 
-char *TAG = "BLE-Server";
-uint8_t ble_addr_type;
-void ble_app_advertise(void);
+#define WIFI_SSID "ESP32_AP"
+#define WIFI_PASSWORD "password123"
+#define MAX_STA_CONN 4
+#define PORT 8080
 
-// Write data to ESP32 defined as server
-static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
-{
-    printf("Data from the client: %.*s\n", ctxt->om->om_len, ctxt->om->om_data);
-    
-    // Check if the received message is "ack"
-    if (strncmp((char *)ctxt->om->om_data, "ack", ctxt->om->om_len) == 0)
-    {
-        // Prepare the response message
-        struct os_mbuf *om = ble_hs_mbuf_from_flat("ack", strlen("ack"));
-        // Send the response message
-        ble_gattc_notify_custom(conn_handle, attr_handle, om);
+static const char *TAG = "ESP32_AP";
+
+void wifi_init() {
+
+    esp_netif_create_default_wifi_ap();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = WIFI_SSID,
+            .ssid_len = strlen(WIFI_SSID),
+            .password = WIFI_PASSWORD,
+            .max_connection = MAX_STA_CONN,
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK
+        },
+    };
+
+    if (strlen(WIFI_PASSWORD) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
-    
-    return 0;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 }
 
-// Read data from ESP32 defined as server
-static int device_read(uint16_t con_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
-{
-    os_mbuf_append(ctxt->om, "Data from the server", strlen("Data from the server"));
-    return 0;
-}
+void scan_wifi_and_send(int sock) {
+    wifi_scan_config_t scan_config = {
+        .ssid = NULL,
+        .bssid = NULL,
+        .channel = 0,
+        .show_hidden = true
+    };
+    ESP_ERROR_CHECK(esp_wifi_scan_start(&scan_config, true));
 
-// Array of pointers to other service definitions
-// UUID - Universal Unique Identifier
-static const struct ble_gatt_svc_def gatt_svcs[] = {
-    {.type = BLE_GATT_SVC_TYPE_PRIMARY,
-     .uuid = BLE_UUID16_DECLARE(0x180),                 // Define UUID for device type
-     .characteristics = (struct ble_gatt_chr_def[]){
-         {.uuid = BLE_UUID16_DECLARE(0xFEF4),           // Define UUID for reading
-          .flags = BLE_GATT_CHR_F_READ,
-          .access_cb = device_read},
-         {.uuid = BLE_UUID16_DECLARE(0xDEAD),           // Define UUID for writing
-          .flags = BLE_GATT_CHR_F_WRITE,
-          .access_cb = device_write},
-         {0}}},
-    {0}};
+    uint16_t num_networks = 0;
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&num_networks));
+    wifi_ap_record_t *ap_records = malloc(sizeof(wifi_ap_record_t) * num_networks);
+    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&num_networks, ap_records));
 
-// BLE event handling
-static int ble_gap_event(struct ble_gap_event *event, void *arg)
-{
-    switch (event->type)
-    {
-    // Advertise if connected
-    case BLE_GAP_EVENT_CONNECT:
-        ESP_LOGI("GAP", "BLE GAP EVENT CONNECT %s", event->connect.status == 0 ? "OK!" : "FAILED!");
-        if (event->connect.status != 0)
-        {
-            ble_app_advertise();
+    char buffer[1024];
+    int len = snprintf(buffer, sizeof(buffer), "Detected %d Wi-Fi networks:\n", num_networks);
+    for (int i = 0; i < num_networks; i++) {
+        len += snprintf(buffer + len, sizeof(buffer) - len,
+                        "SSID: %s, RSSI: %d dBm\n",
+                        ap_records[i].ssid,
+                        ap_records[i].rssi);
+        if (len >= sizeof(buffer)) {
+            break;
         }
-        break;
-    // Advertise again after completion of the event
-    case BLE_GAP_EVENT_ADV_COMPLETE:
-        ESP_LOGI("GAP", "BLE GAP EVENT");
-        ble_app_advertise();
-        break;
-    default:
-        break;
     }
-    return 0;
+
+    send(sock, buffer, strlen(buffer), 0);
+    free(ap_records);
 }
 
-// Define the BLE connection
-void ble_app_advertise(void)
-{
-    // GAP - device name definition
-    struct ble_hs_adv_fields fields;
-    const char *device_name;
-    memset(&fields, 0, sizeof(fields));
-    device_name = ble_svc_gap_device_name(); // Read the BLE device name
-    fields.name = (uint8_t *)device_name;
-    fields.name_len = strlen(device_name);
-    fields.name_is_complete = 1;
-    ble_gap_adv_set_fields(&fields);
+void tcp_server_task(void *pvParameters) {
+    wifi_init(); // Inicializar Wi-Fi
 
-    // GAP - device connectivity definition
-    struct ble_gap_adv_params adv_params;
-    memset(&adv_params, 0, sizeof(adv_params));
-    adv_params.conn_mode = BLE_GAP_CONN_MODE_UND; // connectable or non-connectable
-    adv_params.disc_mode = BLE_GAP_DISC_MODE_GEN; // discoverable or non-discoverable
-    ble_gap_adv_start(ble_addr_type, NULL, BLE_HS_FOREVER, &adv_params, ble_gap_event, NULL);
+    struct sockaddr_in server_addr = {
+        .sin_family = AF_INET,
+        .sin_addr.s_addr = inet_addr("192.168.4.1"), // IP fija del AP
+        .sin_port = htons(PORT)
+    };
+
+    int listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (listen_sock < 0) {
+        ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+        vTaskDelete(NULL);
+    }
+
+    if (bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0) {
+        ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        close(listen_sock);
+        vTaskDelete(NULL);
+    }
+
+    if (listen(listen_sock, 1) != 0) {
+        ESP_LOGE(TAG, "Error during listen: errno %d", errno);
+        close(listen_sock);
+        vTaskDelete(NULL);
+    }
+
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        int client_sock = accept(listen_sock, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_sock < 0) {
+            ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+            continue;
+        }
+
+        char recv_buf[64];
+        int len = recv(client_sock, recv_buf, sizeof(recv_buf) - 1, 0);
+        if (len < 0) {
+            ESP_LOGE(TAG, "Error during recv: errno %d", errno);
+            close(client_sock);
+            continue;
+        }
+
+        recv_buf[len] = "\0"; // Null-terminate the received string
+        ESP_LOGI(TAG, "Received %s", recv_buf);
+        if (strcmp(recv_buf, "Ext AP") == 0) {
+            scan_wifi_and_send(client_sock);
+
+            while (1) {
+                len = recv(client_sock, recv_buf, sizeof(recv_buf) - 1, 0);
+                if (len < 0) {
+                    ESP_LOGE(TAG, "Error during recv: errno %d", errno);
+                    break;
+                }
+
+                recv_buf[len] = 0; // Null-terminate the received string
+                char ssid[32], password[64];
+                if (sscanf(recv_buf, "SSID: %31s Password: %63s", ssid, password) == 2) {
+                    // Attempt to connect to the Wi-Fi network
+                    wifi_config_t wifi_config = {
+                        .sta = {
+                            .ssid = "",
+                            .password = ""
+                        },
+                    };
+                    strncpy((char *)wifi_config.sta.ssid, ssid, sizeof(wifi_config.sta.ssid));
+                    strncpy((char *)wifi_config.sta.password, password, sizeof(wifi_config.sta.password));
+
+                    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+                    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+                    ESP_ERROR_CHECK(esp_wifi_start());
+                    ESP_ERROR_CHECK(esp_wifi_connect());
+
+                    // Create a new socket in the new network
+                    int new_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+                    struct sockaddr_in new_addr;
+                    new_addr.sin_family = AF_INET;
+                    new_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+                    new_addr.sin_port = htons(0); // Let the system choose the port
+
+                    if (bind(new_sock, (struct sockaddr *)&new_addr, sizeof(new_addr)) != 0) {
+                        ESP_LOGE(TAG, "New socket unable to bind: errno %d", errno);
+                        close(new_sock);
+                        break;
+                    }
+
+                    socklen_t new_addr_len = sizeof(new_addr);
+                    if (getsockname(new_sock, (struct sockaddr *)&new_addr, &new_addr_len) != 0) {
+                        ESP_LOGE(TAG, "Unable to get socket name: errno %d", errno);
+                        close(new_sock);
+                        break;
+                    }
+
+                    char ip_str[16];
+                    inet_ntop(AF_INET, &new_addr.sin_addr, ip_str, sizeof(ip_str));
+                    int new_port = ntohs(new_addr.sin_port);
+
+                    send(client_sock, ip_str, strlen(ip_str), 0);
+                    send(client_sock, &new_port, sizeof(new_port), 0);
+
+                    // Disable AP mode
+                    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+                    close(new_sock);
+                    break;
+                }
+            }
+        }
+        else{
+            ESP_LOGE(TAG, "Invalid command received: %s", recv_buf);
+        }
+
+        close(client_sock);
+    }
+
+    close(listen_sock);
+    vTaskDelete(NULL);
 }
 
-// The application
-void ble_app_on_sync(void)
-{
-    ble_hs_id_infer_auto(0, &ble_addr_type); // Determines the best address type automatically
-    ble_app_advertise();                     // Define the BLE connection
-}
+void app_main(void) {
 
-// The infinite task
-void host_task(void *param)
-{
-    nimble_port_run(); // This function will return only when nimble_port_stop() is executed
-}
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
-void app_main()
-{
-    nvs_flash_init();                          // 1 - Initialize NVS flash using
-    esp_nimble_hci_init();      // 2 - Initialize ESP controller
-    nimble_port_init();                        // 3 - Initialize the host stack
-    ble_svc_gap_device_name_set("BLE-Server"); // 4 - Initialize NimBLE configuration - server name
-    ble_svc_gap_init();                        // 4 - Initialize NimBLE configuration - gap service
-    ble_svc_gatt_init();                       // 4 - Initialize NimBLE configuration - gatt service
-    ble_gatts_count_cfg(gatt_svcs);            // 4 - Initialize NimBLE configuration - config gatt services
-    ble_gatts_add_svcs(gatt_svcs);             // 4 - Initialize NimBLE configuration - queues gatt services.
-    ble_hs_cfg.sync_cb = ble_app_on_sync;      // 5 - Initialize application
-    nimble_port_freertos_init(host_task);      // 6 - Run the thread
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    xTaskCreate(tcp_server_task, "tcp_server", 4096*2, NULL, 5, NULL);
 }
