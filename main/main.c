262314dcb1984b5cfaba6331d5e19e4532ab93b8
SantiRecalde <santi.recalde@mi.unc.edu.ar>
2025-02-05 21:36:37 -0300

    Se comprobó una variación en la cantidad de datos recibidos, se harán más pruebas

--- main/main.c
+++ main/main.c
@@ -30,16 +30,16 @@ void i2s_adc_init()
     // Configurar el I2S en modo ADC
     i2s_config_t i2s_config = {
         .mode = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN,
-        .sample_rate = SAMPLE_RATE,
+        .sample_rate = SAMPLE_RATE*6,
         .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
         .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
         .communication_format = I2S_COMM_FORMAT_I2S_MSB,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
         .dma_buf_count = 2,
         .dma_buf_len = 1024,
-        .use_apll = false,
-        .tx_desc_auto_clear = false,
-        .fixed_mclk = 0
+        .use_apll = true,
+        .tx_desc_auto_clear = true,
+        .fixed_mclk = 70000000
     };
 
     // Instalar y configurar el driver I2S
