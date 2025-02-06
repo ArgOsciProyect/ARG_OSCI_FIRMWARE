0542211aeead9fb62186ba5cca60630abaf405c4
SantiRecalde <santi.recalde@mi.unc.edu.ar>
2025-02-06 12:51:25 -0300

    Hay que probar PWM y sincronización con SCLK

--- main/main.c
+++ main/main.c
@@ -10,46 +10,59 @@
 #include "esp_netif.h"
 #include "driver/i2s.h"
 #include "driver/adc.h"
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
 #include <math.h>
 
 #define ADC_CHANNEL ADC_CHANNEL_5
 #define SAMPLE_RATE 2000000 // 2 MHz
-#define I2S_NUM         (0)
 #define BUF_SIZE 8192
+#define PIN_NUM_MISO 12
+#define PIN_NUM_MOSI 13
+#define PIN_NUM_CLK  14
+#define PIN_NUM_CS   15
 
 static const char *TAG = "ARG_OSCI";
 int read_miss_count = 0;
 uint32_t samples_p_second = 0;
 
-void i2s_adc_init()
+void spi_master_init()
 {
-    // Configurar el ADC
-    adc1_config_width(ADC_WIDTH_BIT_12);
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN_DB_12);
+    esp_err_t ret;
 
-    // Configurar el I2S en modo ADC
-    i2s_config_t i2s_config = {
-        .mode = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN,
-        .sample_rate = SAMPLE_RATE*6,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
-        .communication_format = I2S_COMM_FORMAT_I2S_MSB,
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 2,
-        .dma_buf_len = 1024,
-        .use_apll = true,
-        .tx_desc_auto_clear = true,
-        .fixed_mclk = 70000000
+    // Configurar el bus SPI
+    spi_bus_config_t buscfg = {
+        .miso_io_num = PIN_NUM_MISO,
+        .mosi_io_num = -1,
+        .sclk_io_num = PIN_NUM_CLK,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4096
     };
 
-    // Instalar y configurar el driver I2S
-    i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
+    // Inicializar el bus SPI
+    ret = spi_bus_initialize(HSPI_HOST, &buscfg, 1);
+    ESP_ERROR_CHECK(ret);
 
-    // Configurar el ADC para el I2S
-    i2s_set_adc_mode(ADC_UNIT_1, ADC_CHANNEL);
+    // Configurar el dispositivo SPI
+    spi_device_interface_config_t devcfg = {
+        .clock_speed_hz = 48 * 1000 * 1000, // Velocidad del reloj SPI (10 MHz)
+        .mode = 0,                          // Modo SPI 0
+        .spics_io_num = PIN_NUM_CS,         // Pin CS
+        .queue_size = 7,                    // Tamaño de la cola de transacciones
+        .pre_cb = NULL,                     // Callback antes de cada transacción
+        .post_cb = NULL                     // Callback después de cada transacción
+    };
+
+    // Inicializar el dispositivo SPI
+    spi_device_handle_t spi;
+    ret = spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG, "SPI Master initialized.");
 }
 
-void adc_test(void *pvParameters) {
+void spi_test(void *pvParameters) {
     uint8_t buffer[BUF_SIZE];
     uint32_t len;
     uint32_t sum;
@@ -73,11 +86,12 @@ void adc_test(void *pvParameters) {
                 }
             }
         }
-        i2s_adc_disable(I2S_NUM);
         samples_p_second = sum / 20;
         ESP_LOGI(TAG, "Samples per second: %lu", samples_p_second);
     }
 }
+
+
 
 void app_main() {
     ESP_LOGI(TAG, "Starting ARG_OSCI");
@@ -93,5 +107,5 @@ void app_main() {
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     i2s_adc_init();
 
-    xTaskCreate(adc_test, "adc_test", BUF_SIZE * 1.3, NULL, 5, NULL);
+    xTaskCreate(spi_test, "spi_test", BUF_SIZE * 1.3, NULL, 5, NULL);
 }
