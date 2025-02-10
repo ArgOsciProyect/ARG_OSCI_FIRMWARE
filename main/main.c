7d10dc3c0fc1c7d324279b4b057929e25bba57da
SantiRecalde <santi.recalde@mi.unc.edu.ar>
2025-02-09 21:06:30 -0300

    Configuración con 2.47M de velocidad de transmisión

--- main/main.c
+++ main/main.c
@@ -29,11 +29,13 @@ uint32_t samples_p_second = 0;
 
 // Add to global variables
 static ledc_channel_config_t ledc_channel;
-#define TRIGGER_PWM_FREQ 3 // 25kHz
+static spi_device_handle_t spi;
+#define TRIGGER_PWM_FREQ 3000000 // 25kHz
 #define TRIGGER_PWM_TIMER LEDC_TIMER_0
 #define TRIGGER_PWM_CHANNEL LEDC_CHANNEL_0
 #define TRIGGER_PWM_GPIO GPIO_NUM_26      // Choose appropriate GPIO
 #define TRIGGER_PWM_RES LEDC_TIMER_4_BIT // 4-bit resolution (0-16)
+#define TRIGGER_PWM_DUTY 31.25          // 31.25% duty cycle
 
 void init_trigger_pwm(void)
 {
@@ -53,7 +55,11 @@ void init_trigger_pwm(void)
     ledc_channel.hpoint = 0;
     ledc_channel.timer_sel = TRIGGER_PWM_TIMER;
     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, TRIGGER_PWM_DUTY);
+    ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
 }
+
 
 void spi_master_init()
 {
@@ -66,11 +72,11 @@ void spi_master_init()
         .sclk_io_num = PIN_NUM_CLK,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
-        .max_transfer_sz = 4096
+        .max_transfer_sz = BUF_SIZE
     };
 
     // Inicializar el bus SPI
-    ret = spi_bus_initialize(HSPI_HOST, &buscfg, 1);
+    ret = spi_bus_initialize(HSPI_HOST, &buscfg, 3);
     ESP_ERROR_CHECK(ret);
 
     // Configurar el dispositivo SPI
@@ -78,13 +84,13 @@ void spi_master_init()
         .clock_speed_hz = 48 * 1000 * 1000, // Velocidad del reloj SPI (10 MHz)
         .mode = 0,                          // Modo SPI 0
         .spics_io_num = PIN_NUM_CS,         // Pin CS
-        .queue_size = 7,                    // Tamaño de la cola de transacciones
+        .queue_size = BUF_SIZE,                    // Tamaño de la cola de transacciones
         .pre_cb = NULL,                     // Callback antes de cada transacción
         .post_cb = NULL                     // Callback después de cada transacción
     };
 
     // Inicializar el dispositivo SPI
-    spi_device_handle_t spi;
+
     ret = spi_bus_add_device(HSPI_HOST, &devcfg, &spi);
     ESP_ERROR_CHECK(ret);
 
@@ -93,6 +99,10 @@ void spi_master_init()
 
 void spi_test(void *pvParameters) {
     uint8_t buffer[BUF_SIZE];
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t)); // Clear the transaction structure
+    t.length = BUF_SIZE * 8;  // Length in bits
+    t.rx_buffer = buffer;     // Pointer to the buffer to receive data
     uint32_t len;
     uint32_t sum;
     const int duration_ms = 10000;  // Duración total de ejecución en milisegundos (10 segundos)
@@ -101,26 +111,37 @@ void spi_test(void *pvParameters) {
     while (1) {
         sum = 0;
         start_time = xTaskGetTickCount(); // Marca el tiempo de inicio
-        i2s_adc_enable(I2S_NUM);
+        // Poner en cola la transacción
+        esp_err_t ret = spi_device_queue_trans(spi, &t, 1000 / portTICK_PERIOD_MS);
+        ESP_ERROR_CHECK(ret);
+
         while ((xTaskGetTickCount() - start_time) < pdMS_TO_TICKS(duration_ms)){
-            int ret = i2s_read(I2S_NUM, buffer, BUF_SIZE, &len, 1000 / portTICK_PERIOD_MS);
-            if (ret == ESP_OK && len > 0) {
-                sum += len;
-            } else {
-                read_miss_count++;
-                ESP_LOGW(TAG, "Missed ADC readings! Count: %d", read_miss_count);
-                if (read_miss_count >= 10) {  
-                    ESP_LOGE(TAG, "Critical ADC data loss detected.");
-                    read_miss_count = 0;
-                }
-            }
+             
+             // Obtener el resultado de la transacción
+             spi_transaction_t *rtrans;
+             ret = spi_device_get_trans_result(spi, &rtrans, 1000 / portTICK_PERIOD_MS);
+             ESP_ERROR_CHECK(ret);
+
+             // Poner en cola la transacción
+             esp_err_t ret = spi_device_queue_trans(spi, &t, 1000 / portTICK_PERIOD_MS);
+             ESP_ERROR_CHECK(ret);
+ 
+             if (ret == ESP_OK) {
+                 len = rtrans->rxlength / 8; // rxlength is in bits
+                 sum += len;
+             } else {
+                 read_miss_count++;
+                 ESP_LOGW(TAG, "Missed SPI readings! Count: %d", read_miss_count);
+                 if (read_miss_count >= 10) {
+                     ESP_LOGE(TAG, "Critical SPI data loss detected.");
+                     read_miss_count = 0;
+                 }
+             }
         }
         samples_p_second = sum / 20;
         ESP_LOGI(TAG, "Samples per second: %lu", samples_p_second);
     }
 }
-
-
 
 void app_main() {
     ESP_LOGI(TAG, "Starting ARG_OSCI");
@@ -135,5 +156,7 @@ void app_main() {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     spi_master_init();
+    init_trigger_pwm();
 
+    xTaskCreate(spi_test, "spi_test", BUF_SIZE * 2.6, NULL, 5, NULL);
 }
